f0e0a2644a2dd2249e86d308d9a03079afd240e7
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-22 14:31:15 +0530

    inject 3rd party dependency named guicy.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import org.jasypt.util.text.BasicTextEncryptor;
+
 public class Main {
     public static void main(String[] args) {
-        Starter.main(args);
+        //Starter.main(args);
+
+        String myPw = "hello@1234";
+        String key = "2525";
+
+        BasicTextEncryptor basicTextEncryptor = new BasicTextEncryptor();
+        basicTextEncryptor.setPassword(key);
+
+        String encrypt = basicTextEncryptor.encrypt(myPw);
+        System.out.println(encrypt);
+
+        String decrypt = basicTextEncryptor.decrypt(encrypt);
+        System.out.println(decrypt);
     }
 }
--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,16 +1,28 @@
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import config.AppModule;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
+
+    Injector injector;
+
     public static void main(String[] args) {
         launch();
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
         stage.show();
         stage.setTitle("Dashboard - SMD");
     }
+
+    @Override
+    public void init(){
+        injector = Guice.createInjector(new AppModule());
+}
 }
